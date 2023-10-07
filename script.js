d9a2b0866b2332a346961378c2e74f7475a49f7c
Fr1S1ngCode <62840250+Dsean0212@users.noreply.github.com>
2023-10-07 22:02:47 +0800

    popup function
    debug needed

--- script.js
+++ script.js
@@ -17,16 +17,26 @@ const ladderPositions = [
   { start: 25, end: 31 },
 ];
 
-var player1_coin = document.createElement("div");
-player1_coin.setAttribute("id", "player_coin1");
-player1_coin.innerText = "P1";
+var player_coin = document.createElement("div");
+player_coin.setAttribute("id", "player_coin");
+player_coin.innerText = "TEST";
 
-var player2_coin = document.createElement("div");
-player2_coin.setAttribute("id", "player_coin2");
-player2_coin.innerText = "P2";
+//var player1_coin = document.createElement("div");
+//player1_coin.setAttribute("id", "player_coin1");
+//player1_coin.innerText = "P1";
 
-var current_player = true;
-var player_counter = [0, 0, 0];
+//var player2_coin = document.createElement("div");
+//player2_coin.setAttribute("id", "player_coin2");
+//player2_coin.innerText = "P2";
+
+//var current_player = true;
+//var player_counter = [0, 0, 0];
+
+// Old
+var player_counter = [0, 0];
+
+// New
+var global_counter = 0;
 
 window.addEventListener("load", start);
 
@@ -36,7 +46,9 @@ function start() {
 
 function dice_rolled() {
   dice_number.innerText = random();
-  append_element(player_picker());
+  //append_element(player_picker());
+  player_name.innerHTML = "Player";
+  append_element();
 }
 
 function random() {
@@ -47,6 +59,7 @@ function random() {
   return random_number;
 }
 
+/*
 function player_picker() {
   if (current_player) {
     current_player = false;
@@ -57,29 +70,47 @@ function player_picker() {
     player_name.innerText = "Player 1";
     return 2;
   }
-}
+}*/
 
+
+// Old
+/*function id_creator(num) {
+  return "box_" + num;
+}*/
+
+// New
 function id_creator(num) {
   return "box_" + num;
 }
 
-function coin_id_creator(num) {
+/*function coin_id_creator(num) {
   var string = "player_coin";
   string = string + num;
   return string;
-}
+}*/
 
+/*
 function counter(player) {
   if (player_counter[player] + Number(dice_number.innerText) > 36) {
-    /* do nothing */
+    // do nothing 
   } else {
     player_counter[player] =
       player_counter[player] + Number(dice_number.innerText);
   }
+}*/
+
+// Do calculation
+function counter() {
+  if (global_counter + Number(dice_number.innerText) > 36) {
+    global_counter = 36;
+  } else {
+    global_counter = global_counter + Number(dice_number.innerText);
+  }
 }
 
+/*
 function append_element(player) {
-  counter(player);
+   (player);
   console.log(player_counter[player]);
   var player_next_position = document.getElementById(
     id_creator(player_counter[player])
@@ -90,8 +121,23 @@ function append_element(player) {
     player_next_position.append(player2_coin);
   }
   snake_or_ladder(player_counter[player], player);
+}*/
+
+// New
+function append_element() {
+ //(player);
+ counter();
+ popFunction();
+ console.log(global_counter);
+ var player_next_position = document.getElementById(
+   id_creator(global_counter)
+ );
+
+ player_next_position.append(player_coin);
+ snake_or_ladder(global_counter);
 }
 
+/*
 function snake_or_ladder(counter, player) {
   for (i = 0; i < snakePositions.length; i++) {
     const { start, end } = snakePositions[i];
@@ -114,15 +160,120 @@ function snake_or_ladder(counter, player) {
   if (counter == 36) {
     alert(`Player ${player} won the game`);
   }
+}*/
+
+
+// New
+function snake_or_ladder(counter) {
+  for (i = 0; i < snakePositions.length; i++) {
+    const { start, end } = snakePositions[i];
+    if (counter == start) {
+      global_counter = end;
+      after_snake_or_ladder();
+      alerts.innerText = `Player got snake to: ${end}`;
+    }
+  }
+
+  for (j = 0; j < ladderPositions.length; j++) {
+    const { start, end } = ladderPositions[j];
+    if (counter == start) {
+      global_counter = end;
+      after_snake_or_ladder();
+      alerts.innerText = `Player got ladder to: ${end}`;
+    }
+  }
+
+  if (counter >= 36) {
+    alert(`Player won the game`);
+  }
 }
 
-function after_snake_or_ladder(player) {
+function after_snake_or_ladder() {
+
   var player_next_position = document.getElementById(
-    id_creator(player_counter[player])
+    id_creator(global_counter)
   );
-  if (player == 1) {
-    player_next_position.append(player1_coin);
-  } else {
-    player_next_position.append(player2_coin);
+  
+  player_next_position.append(player_coin);
+  
+}
+
+function popFunction() {
+
+  if (global_counter >= 1) {
+
+    checkTable(global_counter);
+    console.log(planet_Dt);
+
+    var popup = document.getElementById("myPopup");
+    popup.innerText=JSON.stringify(planet_Dt[planet_Dt.length-1]["Act"]); 
+    //popup.classList.toggle("show");
+    popup.style.visibility = "visible" ;
+    popup.style['-webkit-animation']= 'fadeIn 1s';
+    popup.style['animation']= 'fadeIn 1s';
+
+    document.getElementById("myImg").src="https://www.nasa.gov/wp-content/uploads/2017/03/psychelongshot0718b_1041x805.jpg";
+    getModal();
+
   }
+}
+
+function getModal() {
+
+   // Get the modal
+   var modal = document.getElementById("myModal");
+
+   // Get the image and insert it inside the modal - use its "alt" text as a caption
+   var img = document.getElementById("myImg");
+   var modalImg = document.getElementById("img01");
+   var captionText = document.getElementById("caption");
+   img.onclick = function(){
+     modal.style.display = "block";
+     modalImg.src = this.src;
+     captionText.innerHTML = this.alt;
+   }
+
+   // Get the <span> element that closes the modal
+   var span = document.getElementsByClassName("close")[0];
+
+   // When the user clicks on <span> (x), close the modal
+   span.onclick = function() { 
+     modal.style.display = "none";
+   }
+
+}
+
+
+var planet_Dt = [];
+var change = 0;
+
+function checkTable(gl_num){
+
+  d3.csv("test.csv").then(rawData =>{
+
+      rawData.forEach(function(d){
+
+        //data for map
+        d.num = Number(d.num);
+        d.Activity = String(d.Activity);
+        d.Location=String(d.Location);
+        d.Description=Number(d.Description);
+
+        if(gl_num == d.num){
+          planet_Dt.push({Act: d.Activity, Loc: d.Location, Desc: d.Description});
+          change = 1;
+        }
+      });
+
+    }//end of csv load.
+
+  ).catch(function(error){
+        console.log(error);
+  });
+
+  if(!change){
+    planet_Dt.push({Act: "Upcoming", Loc: NaN, Desc: NaN});
+    change = 0;
+  }
+
 }
